57422954ef82112c65a97084b1d0b1070dee49c2
thinkty <thinkty@naver.com>
2020-07-27 16:04:43 +0900

    Lint check

--- src/configs/graph.js
+++ src/configs/graph.js
@@ -1,16 +1,16 @@
 /**
  * Configuration file for the SVGs available in the react-digraph component.
  * To add a new node or edge shape, follow the steps below.
- * 
+ *
  * 1. Declare the node/edge type
  * 2. (optional) Specify the dimensions for the new type
  * 3. Draw the shape in SVG
  * 4. Modify the export to include the new type
  */
 
+import React from 'react';
 
 export const NODE_KEY = 'id';
-
 
 // Declaration of node types and edge types
 
@@ -21,7 +21,6 @@ export const BASIC_EDGE = 'basicEdge';
 export const nodeTypes = [INTENT_TYPE, CONTEXT_TYPE];
 export const edgeTypes = [BASIC_EDGE];
 
-
 // Dimensions for each types
 
 const INTENT_WIDTH = 100;
@@ -30,27 +29,25 @@ const INTENT_VIEWBOX = `0 0 ${INTENT_WIDTH} ${INTENT_HEIGHT}`;
 const CONTEXT_RADIUS = 40;
 const CONTEXT_VIEWBOX = `0 0 ${CONTEXT_RADIUS + 2} ${CONTEXT_RADIUS + 2}`;
 
-
 // Shape specifications for each types
 
 const intentNodeShape = (
   <symbol viewBox={INTENT_VIEWBOX} id="intentNode">
-    <rect width={INTENT_WIDTH} height={INTENT_HEIGHT}/>
+    <rect width={INTENT_WIDTH} height={INTENT_HEIGHT} />
   </symbol>
 );
 
 const contextNodeShape = (
   <symbol viewBox={CONTEXT_VIEWBOX} id="contextNode">
-    <circle cx={CONTEXT_RADIUS + 1} cy={CONTEXT_RADIUS + 1} r={CONTEXT_RADIUS}/>
+    <circle cx={CONTEXT_RADIUS + 1} cy={CONTEXT_RADIUS + 1} r={CONTEXT_RADIUS} />
   </symbol>
 );
 
 const basicEdgeShape = (
   <symbol viewBox="0 0 50 50" id="basicEdge">
-    <circle cx="25" cy="25" r="0"/>
+    <circle cx="25" cy="25" r="0" />
   </symbol>
 );
-
 
 // Export
 
@@ -58,17 +55,17 @@ export default {
   EdgeTypes: {
     basicEdge: {
       shape: basicEdgeShape,
-      shapeId: '#basicEdge'
-    }
+      shapeId: '#basicEdge',
+    },
   },
   NodeTypes: {
     intentNode: {
       shape: intentNodeShape,
-      shapeId: '#intentNode'
+      shapeId: '#intentNode',
     },
     contextNode: {
       shape: contextNodeShape,
-      shapeId: '#contextNode'
-    }
-  }
-}
+      shapeId: '#contextNode',
+    },
+  },
+};
