0cf9de85e188cc7ee8bff360d5402f5dbed158e2
thinkty <thinkty@naver.com>
2020-07-27 17:04:55 +0900

    Added NodeEditor barebones

---
+++ src/NodeEditor/NodeEditor.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+/**
+ * Component to edit the currently selected node
+ */
+export default class NodeEditor extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
