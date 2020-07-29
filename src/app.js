935ecfae262531dd027e9ed7907c11549d580424
thinkty <thinkty@naver.com>
2020-07-29 12:46:43 +0900

    Migrate from index.js to app.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DialogflowEditor from './DialogflowEditor';
+
+ReactDOM.render(
+  <div
+    style={{
+      margin: -8,
+    }}
+  >
+    <DialogflowEditor />
+  </div>,
+  document.getElementById('root'),
+);
