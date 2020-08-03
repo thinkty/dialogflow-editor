6721b4ff5ff7cf474600ca656e90c6160135b931
thinkty <thinkty@naver.com>
2020-08-03 12:47:37 +0900

    Distinguishing between context and intent node

--- src/NodeEditor/NodeEditor.js
+++ src/NodeEditor/NodeEditor.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import UneditableTextInput from './UneditableTextInput';
-import { Space } from 'antd';
+import { Menu, Space } from 'antd';
+import { CONTEXT_TYPE, INTENT_TYPE } from '../configs/graph';
 
 /**
  * Component to edit the currently selected node
@@ -42,12 +43,48 @@ export default class NodeEditor extends Component {
 
     const { id, type } = this.state.selected;
 
-
-    return (
-      <Space direction="vertical">
-        <UneditableTextInput value={id} label="Id" />
-        <UneditableTextInput value={type} label="Type" />
-      </Space>
-    );
+    // Distinguish between context type and intent type
+    if (type === CONTEXT_TYPE) {
+      return (
+        <Space 
+          direction="vertical"
+          style={{
+            width: '100%',
+            padding: 20
+          }}
+        >
+          <UneditableTextInput value={id} label="Id" />
+          <UneditableTextInput value={type} label="Type" />
+        </Space>
+      );
+    } else if (type === INTENT_TYPE) {
+      return (
+        <Menu
+          mode="inline"
+          style={{
+            width: '100%'
+          }}
+        >
+          <Menu.SubMenu
+            key="uneditable"
+            title="Node Data"
+          >
+            <Menu.Item key="id">
+              <UneditableTextInput value={id} label="Id" />
+            </Menu.Item>
+            <Menu.Item key="type">
+              <UneditableTextInput value={type} label="Type" />
+            </Menu.Item>
+          </Menu.SubMenu>
+          <Menu.SubMenu
+  
+          >
+  
+          </Menu.SubMenu>
+        </Menu>
+      );
+    } else {
+      return null;
+    }
   }
 }
