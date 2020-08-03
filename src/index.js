8f4f6c4b77b3f16f5354184aef3db238913e55f0
thinkty <thinkty@naver.com>
2020-08-03 10:00:34 +0900

    Renamed app.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DialogflowEditor from './DialogflowEditor';
+import DialogflowEditor from './DialogflowEditor/DialogflowEditor';
 
 ReactDOM.render(
   <div
