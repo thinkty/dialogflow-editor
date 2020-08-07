c5093438db1a628a8b594aad5cb2ace3333e331a
thinkty <thinkty@naver.com>
2020-08-07 19:25:57 +0900

    Added tests for import modal and export modal

--- src/__tests__/EditorMenu.test.js
+++ src/__tests__/EditorMenu.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import '@testing-library/jest-dom';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import EditorMenu from '../EditorMenu';
+import ExportModal from '../EditorMenu/ExportModal';
+import ImportModal from '../EditorMenu/ImportModal';
 
 describe('Editor Menu Smoke Test', () => {
 
@@ -88,4 +90,118 @@ describe('Editor Menu Component', () => {
 
     expect(downloadGraph).toHaveBeenCalledTimes(1);
   });
+});
+
+
+describe('Export Modal Component', () => {
+
+  const visible = true;
+  const agent = 'tempAgent';
+  const flowchart = 'tempFlowchart';
+  const onCancel = jest.fn();
+  const graph = {
+    nodes: ['item', 'item'],
+    edges: ['item'],
+  };
+  let output;
+
+  beforeEach(() => {
+    output = render(
+      <ExportModal
+        visible={visible}
+        agent={agent}
+        flowchart={flowchart}
+        onCancel={onCancel}
+        graph={graph}
+      />
+    );
+  });
+
+  it('renders without crashing', () => {
+    mount(
+      <ExportModal
+        visible={visible}
+        agent={agent}
+        flowchart={flowchart}
+        onCancel={onCancel}
+        graph={graph}
+      />
+    );
+  });
+
+  it('renders the modal with appropriate input', () => {
+    const initialValue = 'www.example.com';
+    const { getAllByRole } = output;
+    const inputs = getAllByRole('textbox');
+    let exists = false;
+
+    inputs.forEach((input) => {
+      if (input.hasAttribute('value') && input.value === initialValue) {
+        exists = true;
+      }
+    });
+
+    expect(exists).toBeTruthy();
+  });
+
+  it('makes a request on send clicked', () => {
+    global.fetch = jest.fn(async () => {});
+    const { getAllByRole } = output;
+    const button = getAllByRole('button', { name: 'Send' })[0];
+    
+    fireEvent.click(button);
+    expect(global.fetch).toHaveBeenCalled();
+  });
+
+  it('closes modal on cancel', () => {
+    const { getAllByRole } = output;
+    const button = getAllByRole('button', { name: 'Close' })[0];
+
+    fireEvent.click(button);
+    expect(onCancel).toHaveBeenCalled();
+  });
+});
+
+describe('Import Modal Component', () => {
+
+  const visible = true;
+  const importGraph = jest.fn();
+  const onCancel = jest.fn();
+  let output = render(
+    <ImportModal 
+      visible={visible}
+      importGraph={importGraph}
+      onCancel={onCancel}
+    />
+  );
+
+  beforeEach(() => {
+    output = render(
+      <ImportModal 
+        visible={visible}
+        importGraph={importGraph}
+        onCancel={onCancel}
+      />
+    );
+  });
+
+  it('renders without crashing', () => {
+    mount(
+      <ImportModal 
+        visible={visible}
+        importGraph={importGraph}
+        onCancel={onCancel}
+      />
+    );
+  });
+
+  it('imports file on click', () => {
+    const { getAllByRole } = output;
+    const button = getAllByRole('button', { name: 'Import' })[0];
+    const readAsTextSpy = jest.spyOn(FileReader.prototype, 'readAsText');
+
+    fireEvent.click(button);
+    expect(readAsTextSpy).toBeCalledWith('');
+  });
+
 });
