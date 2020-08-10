8bd9824f2d4fee61bcf7ddc6d98e24abb16ae680
thinkty <thinkty@naver.com>
2020-08-10 16:36:36 +0900

    Specified attributes for each node type

--- src/configs/type.js
+++ src/configs/type.js
@@ -1,3 +1,34 @@
 /**
  * Configuration file for the attributes for each type specified in graph.js
  */
+
+import {
+  INTENT_TYPE,
+  CONTEXT_TYPE,
+} from './graph';
+
+export const generalAttributes = ["id", "title"];
+
+export const nodeAttributes = {
+  [INTENT_TYPE]: {
+    displayName: 'Intent Node',
+    attributes: {
+      type: INTENT_TYPE,
+      contexts: {
+        in: [],
+        out: [],
+      },
+      events: [],
+      trainingPhrases: [],
+      action: '',
+      responses: [],
+      fulfillment: false,
+    },
+  },
+  [CONTEXT_TYPE]: {
+    displayName: 'Context Node',
+    attributes: {
+      type: CONTEXT_TYPE,
+    },
+  },
+};
