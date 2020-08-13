216a714226e7cfe72175ff54c6972194ddf3c92e
thinkty <thinkty@naver.com>
2020-08-13 14:44:49 +0900

    Lint check

--- src/NodeEditor/inputs/SimpleArrayInput.js
+++ src/NodeEditor/inputs/SimpleArrayInput.js
@@ -12,7 +12,7 @@ import PropTypes from 'prop-types';
 export default class SimpleArrayInput extends Component {
   constructor(props) {
     super(props);
-    this.state = { temp: ''};
+    this.state = { temp: '' };
   }
 
   /**
